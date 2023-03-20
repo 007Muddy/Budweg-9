1ea47b8c4d04998bd23fed44caa9cc040e2f2135
Maahdi313 <mr.farhad112@gmail.com>
2023-03-20 10:33:18 +0100

    changes

--- ProjektBudweg/ProjektBudweg/ViewModel/EmployeeViewModel.cs
+++ ProjektBudweg/ProjektBudweg/ViewModel/EmployeeViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjektBudweg.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,14 @@ namespace ProjektBudweg.ViewModel
 {
     public class EmployeeViewModel
     {
+        public int EmployeeID { get; set; }
+        public string Department { get; set; }
+
+
+        public EmployeeViewModel(Employee emp)
+        {
+            EmployeeID = emp.EmployeeID;
+            this.Department = emp.Department;
+        }
     }
 }
